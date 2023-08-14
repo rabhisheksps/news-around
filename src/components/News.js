9f31d2807989654648e8c2d50c88fdb3c63d06ac
rabhisheksps <abhishek_rastogi@softprodigy.com>
2023-08-14 18:25:44 +0530

    code refactoring

--- src/components/News.js
+++ src/components/News.js
@@ -16,60 +16,53 @@ export default class News extends Component {
     category: PropTypes.string
   }
 
-  constructor(){
-    super();
+  capitalizeFirstLetter = (string) => {
+    return string.charAt (0).toUpperCase() + string.slice (1);
+  }
+
+  constructor(props){
+    super(props);
     console.log("Hello, I'm a constructor.")
     this.state = {
       articles: [],
       loading: true,
       page: 1
     }
+    document.title = ` NewsAround - ${this.capitalizeFirstLetter(this.props.category)}`
   }
 
-  async componentDidMount(){
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78de02fb6195495a946b8ed937b46868&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78de02fb6195495a946b8ed937b46868&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({articles: parsedData.articles, 
-        totalResults: parsedData.totalResults,
+      totalResults: parsedData.totalResults,
       loading:false
     });
   }
+  async componentDidMount(){
+   this.updateNews();
+  }
 
   handlePreviousClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78de02fb6195495a946b8ed937b46868&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
-    this.setState({loading: true});
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page - 1,
-      loading: false
-    })
-
+    this.setState({page: this.state.page - 1})
+    this.updateNews();
   }
   handleNextClick = async () => {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=78de02fb6195495a946b8ed937b46868&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
-    this.setState({loading: true});
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    this.setState({
-      articles: parsedData.articles,
-      page: this.state.page + 1,
-      loading: false
-    })
+    this.setState({page: this.state.page + 1})
+    this.updateNews();
   }  
   
   render() {
     return (
       <div className='container my-3'>
-        <h1 className='text-center'>NewsAround - Top Headlines </h1>
+        <h1 className='text-center'>NewsAround - Top {this.capitalizeFirstLetter(this.props.category)} Headlines </h1>
         {this.state.loading && <Spinner />}
         <div className="row my-3">
           {!this.state.loading && this.state.articles.map((element) => {
             return   <div className="col-md-4" key={element.url}>
-            <NewsItem key={element.url} title={element.title?element.title:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            <NewsItem key={element.url} title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt}/>
           </div>
           })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,18 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, imageUrl, newsUrl} = this.props;
+    let {title, imageUrl,description, newsUrl, author, date} = this.props;
     return (
       <div className='my-3'>
-        <div className="news-card">
-          <img src={!imageUrl?"https://cdn.vox-cdn.com/thumbor/Ivub7G4vbkR9T9YV2AtI6rtqwIs=/0x0:1162x662/1200x628/filters:focal(581x331:582x332)/cdn.vox-cdn.com/uploads/chorus_asset/file/24838915/penn_espn_bet.jpg":imageUrl} className="card-img-top" style={{width: "100%", height: "10rem"}} alt="..." />
+        <div className="news-card my-4 mx-2" >
+          <img src={!imageUrl?"https://cdn.vox-cdn.com/thumbor/Ivub7G4vbkR9T9YV2AtI6rtqwIs=/0x0:1162x662/1200x628/filters:focal(581x331:582x332)/cdn.vox-cdn.com/uploads/chorus_asset/file/24838915/penn_espn_bet.jpg":imageUrl} className="card-img-top" style={{width: "100%", height: "12rem"}} alt="..." />
           <div className="card-body">
-            <h5 className="card-title" style={{height: "9rem"}}>{title}</h5>
-            {/* <p className="card-text" style={{height: "4rem"}}>{description.slice(0, 95)}...</p> */}
+            {/* <h5 className="card-title" style={{height: "6rem"}}>{title} <span className="position-absolute top-0 start-100 translate-middle
+             badge rounded-pill bg-danger"> {source}
+            </span></h5> */}
+            <h5 className="card-title" style={{height: "auto"}}>{title}</h5>
+            <p className="card-text" style={{height: "auto"}}>{description.slice(0, 95)}...</p>
+            <p className="card-text" style={{height: "auto"}}><small className="text-danger">By {author?author:"Unkown"} on {new Date(date).toGMTString()}</small></p>
             <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
           </div>
         </div>
